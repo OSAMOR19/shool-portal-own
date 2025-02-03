969c30455fbf6257826403d991ab21aad1e8f6c5
Your Name <isaacchukwuka67@gmail.com>
2025-02-03 10:25:39 +0100

    new rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { 
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }],
+      // You can add more rules here as needed
+    },
+  },
 ];
 
 export default eslintConfig;
